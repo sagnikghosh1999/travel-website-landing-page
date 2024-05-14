e4a19a22db4bf3ea0eeb6cb61cb85e0cca389ebb
sagnikghosh1999 <saggywebd@gmail.com>
2024-05-14 20:59:50 +0530

    first commmit

---
+++ src/app/page.tsx
@@ -0,0 +1,17 @@
+import Camp from "@/components/camp";
+import Features from "@/components/features";
+import GetApp from "@/components/getapp";
+import Guide from "@/components/guide";
+import Hero from "@/components/hero";
+
+export default function Home() {
+  return (
+    <>
+      <Hero />
+      <Camp />
+      <Guide />
+      <Features />
+      <GetApp />
+    </>
+  );
+}
